4a94a90be44890885987864632537f8542e658eb
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2023-03-04 12:09:52 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,21 +15,21 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
+	/* calculate string length */
 	while (b[len] != '\0')
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 		len++;
 	}
- 	 
+
+	/* convert binary to decimal */
 	for (len--; len >= 0; len--)
 	{
-			
-		if( b[len] == '1' )
-		{
-		num += base_two;
-		}
-        base_two *=2;
+		if (b[len] == '1')
+			num += base_two;
+
+		base_two *= 2;
 	}
 
 	return (num);

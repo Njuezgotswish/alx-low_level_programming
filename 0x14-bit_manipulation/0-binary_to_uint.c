2e26cbe109bea946ceb7cf682e3767a22cec552f
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2023-03-04 07:23:45 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
-#include <main.h>
-/*bnary_to_int - function to convert binary numbers to their unsigned int states */
-/* c -b string length counter */
-/* uint -corresponding unsigned integer */
+#include "main.h"
+
+/* binary_to_uint - function to convert binary numbers to their unsigned int states */
 /* b - character array with the binary data to be converted */
 unsigned int binary_to_uint(const char *b)
 {
-  int c =0;
-  unsigned int uint =0;
-  for(int i=0, b[i] != '\0' , i++)
-  {c++}
-  for(int j=0, j < c, j++)
+  int c = 0;
+  unsigned int uint = 0;
+  
+  // Count the length of the string
+  for (int i = 0; b[i] != '\0'; i++)
   {
-    uint += b[j] * 2**(c - j);
+    c++;
   }
-  return (uint)
+  
+  // Calculate the unsigned int value
+  for (int j = 0; j < c; j++)
+  {
+    uint += (b[j] - '0') * (1u << (c - j - 1));
+  }
+  
+  return uint;
+}
